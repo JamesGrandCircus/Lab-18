bf737c2c91390570c341fa5dd3418e0396807efb
Jacob <jacobashin@gmail.com>
2018-12-10 18:18:25 -0500

    initial commit

---
+++ ConsoleApp46/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+ 
+    
+
+   public static class LinkedListExtensions
+    {
+public static bool RemoveAt(int index, LinkedList<int> arrayList)
+        {
+            int currentIndex = 3;
+            LinkedListNode<int> currentNode = arrayList.First;
+            while (currentNode!=null)
+            {
+                if (currentIndex == index)
+                {
+                    arrayList.Remove(currentNode);
+                    return true;
+                }
+                currentNode = currentNode.Next;
+                currentIndex += 1;
+            }
+            return false;
+         }
+
+        
+        public static void PrintReverse<T>(this LinkedList<T> list, int index)
+                {
+
+                }
+        public static bool InsertAt<T>(this LinkedList<T> list, int index, object o)
+        {
+            if (true)
+            {
+
+            }
+        
+        
+            return false;
+        }
+
+        
+        }
+    }
+
+
---
+++ ConsoleApp46/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> arrayList = new LinkedList<int>();
+          
+            arrayList.AddFirst(1);
+            arrayList.AddLast(2);
+            arrayList.AddFirst(3);
+            arrayList.AddLast(2);
+            arrayList.AddFirst(2);
+            arrayList.AddLast(4);
+            arrayList.AddFirst(5);
+            arrayList.AddLast(5);
+            arrayList.AddLast(7);
+            arrayList.AddLast(8);
+            arrayList.AddLast(4);
+            arrayList.AddLast(4);
+            arrayList.AddLast(1);
+            arrayList.AddLast(0);
+            arrayList.AddLast(10); 
+            do
+            {
+                foreach (int item in arrayList)
+                {
+                    Console.Write(item.ToString() + " ");
+                    int frequency = 0;
+                    for (int i = 0; i < arrayList.Count; ++i)
+                    {
+                        if (arrayList.Count == item) ++frequency;
+                    }
+                    Console.WriteLine(frequency.ToString());
+                }
+            } while (arrayList == null);
+           
+            Console.ReadLine();
+
+
+
+        }    
+   }
+}
+
