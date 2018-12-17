a3cca78b22bc0c0c432309a3f18ca57ae75a0f0e
James Jackson <James@Grandcircus.co>
2018-12-16 21:19:04 -0500

    James TA Review

--- ConsoleApp46/Class1.cs
+++ ConsoleApp46/Class1.cs
@@ -1,51 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ConsoleApp46
 {
- 
-    
 
-   public static class LinkedListExtensions
-    {
-public static bool RemoveAt(int index, LinkedList<int> arrayList)
-        {
-            int currentIndex = 3;
-            LinkedListNode<int> currentNode = arrayList.First;
-            while (currentNode!=null)
-            {
-                if (currentIndex == index)
-                {
-                    arrayList.Remove(currentNode);
-                    return true;
-                }
-                currentNode = currentNode.Next;
-                currentIndex += 1;
-            }
-            return false;
-         }
 
-        
-        public static void PrintReverse<T>(this LinkedList<T> list, int index)
-                {
+	// James -- I'm glad to see that this is in it's own file, but the name of the file is just Class1... to make it clear, please name it something more relevant to what it is...
+	// like LinkedListExtensions.cs
+	public static class LinkedListExtensions
+	{
+		public static bool RemoveAt(int index, LinkedList<int> arrayList)
+		{
+			int currentIndex = 3;
+			LinkedListNode<int> currentNode = arrayList.First;
+			while (currentNode != null)
+			{
+				if (currentIndex == index)
+				{
+					arrayList.Remove(currentNode);
+					return true;
+				}
+				currentNode = currentNode.Next;
+				currentIndex += 1;
+			}
+			return false;
+		}
 
-                }
-        public static bool InsertAt<T>(this LinkedList<T> list, int index, object o)
-        {
-            if (true)
-            {
 
-            }
-        
-        
-            return false;
-        }
+		public static void PrintReverse<T>(this LinkedList<T> list, int index)
+		{
 
-        
-        }
-    }
+		}
+		public static bool InsertAt<T>(this LinkedList<T> list, int index, object o)
+		{
+			if (true)
+			{
+
+			}
+
+
+			return false;
+		}
+
+
+	}
+}
 
 
--- ConsoleApp46/Program.cs
+++ ConsoleApp46/Program.cs
@@ -1,51 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp46
 {
-   public class Program
-    {
-        public static void Main(string[] args)
-        {
-            LinkedList<int> arrayList = new LinkedList<int>();
-          
-            arrayList.AddFirst(1);
-            arrayList.AddLast(2);
-            arrayList.AddFirst(3);
-            arrayList.AddLast(2);
-            arrayList.AddFirst(2);
-            arrayList.AddLast(4);
-            arrayList.AddFirst(5);
-            arrayList.AddLast(5);
-            arrayList.AddLast(7);
-            arrayList.AddLast(8);
-            arrayList.AddLast(4);
-            arrayList.AddLast(4);
-            arrayList.AddLast(1);
-            arrayList.AddLast(0);
-            arrayList.AddLast(10); 
-            do
-            {
-                foreach (int item in arrayList)
-                {
-                    Console.Write(item.ToString() + " ");
-                    int frequency = 0;
-                    for (int i = 0; i < arrayList.Count; ++i)
-                    {
-                        if (arrayList.Count == item) ++frequency;
-                    }
-                    Console.WriteLine(frequency.ToString());
-                }
-            } while (arrayList == null);
-           
-            Console.ReadLine();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			LinkedList<int> arrayList = new LinkedList<int>();
+
+			arrayList.AddFirst(1);
+			arrayList.AddLast(2);
+			arrayList.AddFirst(3);
+			arrayList.AddLast(2);
+			arrayList.AddFirst(2);
+			arrayList.AddLast(4);
+			arrayList.AddFirst(5);
+			arrayList.AddLast(5);
+			arrayList.AddLast(7);
+			arrayList.AddLast(8);
+			arrayList.AddLast(4);
+			arrayList.AddLast(4);
+			arrayList.AddLast(1);
+			arrayList.AddLast(0);
+			arrayList.AddLast(10);
+			do
+			{
+				// James -- I'm not sure what you think  you are doing with this foreach loop, but it is not doing what you think it is doing 
+				foreach (int item in arrayList)
+				{
+					Console.Write(item.ToString() + " ");
+					int frequency = 0;
+					for (int i = 0; i < arrayList.Count; ++i)
+					{
+						if (arrayList.Count == item) ++frequency;
+					}
+					Console.WriteLine(frequency.ToString());
+				}
+			} while (arrayList == null);
+
+			Console.ReadLine();
 
 
 
-        }    
-   }
+		}
+	}
 }
 
